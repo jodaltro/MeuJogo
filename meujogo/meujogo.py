2938ae48aae5917a3e5797e8b6d67442fcdc755c
Jodaltro Ricco Cardoso <jodaltrocardoso@gmail.com>
2020-11-22 23:20:52 -0300

    Update meujogo.py

--- meujogo/meujogo.py
+++ meujogo/meujogo.py
@@ -1,3 +1,591 @@
+from __future__ import division
+
+import sys
+import math
+import random
+import time
+
+from collections import deque
+from pyglet import image
+from pyglet.gl import *
+from pyglet.graphics import TextureGroup
+from pyglet.window import key, mouse
+
+TICKS_PER_SEC = 60
+
+TAMANHO_SETOR = 16
+
+CAMINHAR_VELOCIDADE = 5
+VOAR_VELOCIDADE = 15
+
+GRAVIDADE = 20.0
+MAX_ALTURA_PULO = 3.0 
+
+PULO_VELOCIDADE = math.sqrt(2 * GRAVIDADE * MAX_ALTURA_PULO)
+TERMINAL_VELOCIDADE = 50
+
+ALTURA_JOGADOR = 2
+
+if sys.version_info[0] >= 3:
+    xrange = range
+
+def cube_vertices(x, y, z, n):
+    return [
+        x-n,y+n,z-n, x-n,y+n,z+n, x+n,y+n,z+n, x+n,y+n,z-n,  
+        x-n,y-n,z-n, x+n,y-n,z-n, x+n,y-n,z+n, x-n,y-n,z+n,  
+        x-n,y-n,z-n, x-n,y-n,z+n, x-n,y+n,z+n, x-n,y+n,z-n,  
+        x+n,y-n,z+n, x+n,y-n,z-n, x+n,y+n,z-n, x+n,y+n,z+n,  
+        x-n,y-n,z+n, x+n,y-n,z+n, x+n,y+n,z+n, x-n,y+n,z+n,  
+        x+n,y-n,z-n, x-n,y-n,z-n, x-n,y+n,z-n, x+n,y+n,z-n,  
+    ]
+
+
+def tex_coord(x, y, n=4):
+
+    m = 1.0 / n
+    dx = x * m
+    dy = y * m
+    return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
+
+
+def tex_coords(top, bottom, side):
+
+    top = tex_coord(*top)
+    bottom = tex_coord(*bottom)
+    side = tex_coord(*side)
+    result = []
+    result.extend(top)
+    result.extend(bottom)
+    result.extend(side * 4)
+    return result
+
+
+TEXTURA_PATH = 'texture.png'
+
+GRAMA = tex_coords((1, 0), (0, 1), (0, 0))
+AREIA = tex_coords((1, 1), (1, 1), (1, 1))
+TIJOLO = tex_coords((2, 0), (2, 0), (2, 0))
+PEDRA = tex_coords((2, 1), (2, 1), (2, 1))
+
+FACES = [
+    ( 0, 1, 0),
+    ( 0,-1, 0),
+    (-1, 0, 0),
+    ( 1, 0, 0),
+    ( 0, 0, 1),
+    ( 0, 0,-1),
+]
+
+
+def normalize(position):
+
+    x, y, z = position
+    x, y, z = (int(round(x)), int(round(y)), int(round(z)))
+    return (x, y, z)
+
+
+def sectorize(position):
+
+    x, y, z = normalize(position)
+    x, y, z = x // TAMANHO_SETOR, y // TAMANHO_SETOR, z // TAMANHO_SETOR
+    return (x, 0, z)
+
+
+class Model(object):
+
+    def __init__(self):
+
+        self.batch = pyglet.graphics.Batch()
+        self.group = TextureGroup(image.load(TEXTURA_PATH).get_texture())
+
+        self.world = {}
+
+        self.cont = 1
+
+        self.shown = {}
+
+        self._shown = {}
+
+        self.sectors = {}
+
+        self.queue = deque()
+
+        self.pX = 0
+        self.pY = -1
+        self.pZ = -1
+
+        self.coordenadasBlocos = deque()
+
+        self.contador = 0
+
+        self.animateTime = 0
+
+    def _initialize(self):
+        
+        n = 80 
+        s = 1 
+        y = 0  
+        for x in xrange(-n, n + 1, s):
+            for z in xrange(-n, n + 1, s):
+                self.add_block((x, y - 2, z), GRAMA, immediate=True)
+                self.add_block((x, y - 3, z), PEDRA, immediate=True)
+                if x in (-n, n) or z in (-n, n):
+                    for dy in xrange(-2, 3):
+                        self.add_block((x, y + dy, z), PEDRA, immediate=False)
+          
+    def posicao_personagem(self):
+        return (self.pX, self.pY, self.pZ)
+
+    def _mover_personagem_frente(self):
+        self.pZ = self.pZ - 1
+
+    def _mover_personagem_atras(self):
+        self.pZ = self.pZ + 1
+
+    def _mover_personagem_acima(self):
+        self.pY = self.pY + 1
+
+    def _mover_personagem_abaixo(self):
+        self.pY = self.pY - 1
+
+    def _mover_personagem_direita(self):
+        self.pX = self.pX + 1
+
+    def _mover_personagem_esquerda(self):
+        self.pX = self.pX - 1
+
+    def _adicionar_bloco(self):
+        self.coordenadasBlocos.append((self.pX, self.pY, self.pZ, 1, bloco))
+
+    def _remover_bloco(self):
+        self.coordenadasBlocos.append((self.pX, self.pY, self.pZ, 0, ''))
+
+    def executarComandos(self):
+        if (self.animateTime == 0):
+            self.animateTime = time.process_time()
+
+        if time.process_time() > self.animateTime and self.coordenadasBlocos:
+            (x, y, z, adicionar, _bloco)  = self.coordenadasBlocos.popleft()
+            if (adicionar):
+                self.add_block((x, y, z), globals()[_bloco], immediate=True)
+            else:
+                self.remove_block((x, y, z), immediate=True)
+            self.animateTime = time.process_time() + 1.0 / 10
+
+    def hit_test(self, position, vector, max_distance=8):
+
+        m = 8
+        x, y, z = position
+        dx, dy, dz = vector
+        previous = None
+        for _ in xrange(max_distance * m):
+            key = normalize((x, y, z))
+            if key != previous and key in self.world:
+                return key, previous
+            previous = key
+            x, y, z = x + dx / m, y + dy / m, z + dz / m
+        return None, None
+
+    def exposed(self, position):
+
+        x, y, z = position
+        for dx, dy, dz in FACES:
+            if (x + dx, y + dy, z + dz) not in self.world:
+                return True
+        return False
+
+    def add_block(self, position, texture, immediate=True):
+
+        if position in self.world:
+            self.remove_block(position, immediate)
+        self.world[position] = texture
+        self.sectors.setdefault(sectorize(position), []).append(position)
+        if immediate:
+            if self.exposed(position):
+                self.show_block(position)
+            self.check_neighbors(position)
+
+    def remove_block(self, position, immediate=True):
+
+        if (position in self.world):
+            del self.world[position]
+            self.sectors[sectorize(position)].remove(position)
+            if immediate:
+                if position in self.shown:
+                    self.hide_block(position)
+                self.check_neighbors(position)
+
+    def check_neighbors(self, position):
+
+        x, y, z = position
+        for dx, dy, dz in FACES:
+            key = (x + dx, y + dy, z + dz)
+            if key not in self.world:
+                continue
+            if self.exposed(key):
+                if key not in self.shown:
+                    self.show_block(key)
+            else:
+                if key in self.shown:
+                    self.hide_block(key)
+
+    def show_block(self, position, immediate=True):
+
+        texture = self.world[position]
+        self.shown[position] = texture
+        if immediate:
+            self._show_block(position, texture)
+        else:
+            self._enqueue(self._show_block, position, texture)
+
+    def _show_block(self, position, texture):
+
+        x, y, z = position
+        vertex_data = cube_vertices(x, y, z, 0.5)
+        texture_data = list(texture)
+        self._shown[position] = self.batch.add(24, GL_QUADS, self.group,
+            ('v3f/static', vertex_data),
+            ('t2f/static', texture_data))
+
+    def hide_block(self, position, immediate=True):
+
+        self.shown.pop(position)
+        if immediate:
+            self._hide_block(position)
+        else:
+            self._enqueue(self._hide_block, position)
+
+    def _hide_block(self, position):
+        self._shown.pop(position).delete()
+
+    def show_sector(self, sector):
+        for position in self.sectors.get(sector, []):
+            if position not in self.shown and self.exposed(position):
+                self.show_block(position, False)
+
+    def hide_sector(self, sector):
+        for position in self.sectors.get(sector, []):
+            if position in self.shown:
+                self.hide_block(position, False)
+
+    def change_sectors(self, before, after):
+        before_set = set()
+        after_set = set()
+        pad = 4
+        for dx in xrange(-pad, pad + 1):
+            for dy in [0]:  # xrange(-pad, pad + 1):
+                for dz in xrange(-pad, pad + 1):
+                    if dx ** 2 + dy ** 2 + dz ** 2 > (pad + 1) ** 2:
+                        continue
+                    if before:
+                        x, y, z = before
+                        before_set.add((x + dx, y + dy, z + dz))
+                    if after:
+                        x, y, z = after
+                        after_set.add((x + dx, y + dy, z + dz))
+        show = after_set - before_set
+        hide = before_set - after_set
+        for sector in show:
+            self.show_sector(sector)
+        for sector in hide:
+            self.hide_sector(sector)
+
+    def _enqueue(self, func, *args):
+        self.queue.append((func, args))
+
+    def _dequeue(self):
+        func, args = self.queue.popleft()
+        func(*args)
+
+    def process_queue(self):
+        start = time.process_time()
+        while self.queue and time.process_time() - start < 1.0 / TICKS_PER_SEC:
+            self._dequeue()
+
+    def process_entire_queue(self):
+        while self.queue:
+            self._dequeue()
+
+
+class Window(pyglet.window.Window):
+
+    def __init__(self, *args, **kwargs):
+        super(Window, self).__init__(*args, **kwargs)
+
+        self.exclusive = False
+        self.flying = False
+
+        self.strafe = [0, 0]
+        self.position = (0, 0, 0)
+        self.rotation = (0, 0)
+
+        self.sector = None
+
+        self.reticle = None
+
+        self.dy = 0
+
+        self.inventory = [TIJOLO, GRAMA, AREIA]
+
+        self.block = self.inventory[0]
+
+        self.num_keys = [
+            key._1, key._2, key._3, key._4, key._5,
+            key._6, key._7, key._8, key._9, key._0]
+
+        self.model = Model()
+
+        self.label = pyglet.text.Label('', font_name='Arial', font_size=18,
+            x=10, y=self.height - 10, anchor_x='left', anchor_y='top',
+            color=(0, 0, 0, 255))
+
+        pyglet.clock.schedule_interval(self.update, 1.0 / TICKS_PER_SEC)
+
+    def set_exclusive_mouse(self, exclusive):
+        super(Window, self).set_exclusive_mouse(exclusive)
+        self.exclusive = exclusive
+
+    def get_sight_vector(self):
+        x, y = self.rotation
+        m = math.cos(math.radians(y))
+        dy = math.sin(math.radians(y))
+        dx = math.cos(math.radians(x - 90)) * m
+        dz = math.sin(math.radians(x - 90)) * m
+        return (dx, dy, dz)
+
+    def get_motion_vector(self):
+        if any(self.strafe):
+            x, y = self.rotation
+            strafe = math.degrees(math.atan2(*self.strafe))
+            y_angle = math.radians(y)
+            x_angle = math.radians(x + strafe)
+            if voar:
+                m = math.cos(y_angle)
+                dy = math.sin(y_angle)
+                if self.strafe[1]:
+                    dy = 0.0
+                    m = 1
+                if self.strafe[0] > 0:
+                    dy *= -1
+                dx = math.cos(x_angle) * m
+                dz = math.sin(x_angle) * m
+            else:
+                dy = 0.0
+                dx = math.cos(x_angle)
+                dz = math.sin(x_angle)
+        else:
+            dy = 0.0
+            dx = 0.0
+            dz = 0.0
+        return (dx, dy, dz)
+
+    def update(self, dt):
+        self.model.process_queue()
+        sector = sectorize(self.position)
+        if sector != self.sector:
+            self.model.change_sectors(self.sector, sector)
+            if self.sector is None:
+                self.model.process_entire_queue()
+            self.sector = sector
+        m = 8
+        dt = min(dt, 0.2)
+        for _ in xrange(m):
+            self._update(dt / m)
+
+        self.model.executarComandos()
+
+    def _update(self, dt):
+        speed = VOAR_VELOCIDADE if voar else CAMINHAR_VELOCIDADE
+        d = dt * speed 
+        dx, dy, dz = self.get_motion_vector()
+        dx, dy, dz = dx * d, dy * d, dz * d
+        if not voar:
+            self.dy -= dt * GRAVIDADE
+            self.dy = max(self.dy, -TERMINAL_VELOCIDADE)
+            dy += self.dy * dt
+        x, y, z = self.position
+        x, y, z = self.collide((x + dx, y + dy, z + dz), ALTURA_JOGADOR)
+        self.position = (x, y, z)
+
+    def criarColinas(self):
+        self.model.criarColinas()
+
+    def collide(self, position, height):
+        pad = 0.25
+        p = list(position)
+        np = normalize(position)
+        for face in FACES:  
+            for i in xrange(3): 
+                if not face[i]:
+                    continue
+                d = (p[i] - np[i]) * face[i]
+                if d < pad:
+                    continue
+                for dy in xrange(height):
+                    op = list(np)
+                    op[1] -= dy
+                    op[i] += face[i]
+                    if tuple(op) not in self.model.world:
+                        continue
+                    p[i] -= (d - pad) * face[i]
+                    if face == (0, -1, 0) or face == (0, 1, 0):
+                        self.dy = 0
+                    break
+        return tuple(p)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+
+        if self.exclusive:
+            vector = self.get_sight_vector()
+            block, previous = self.model.hit_test(self.position, vector)
+            if (button == mouse.RIGHT) or \
+                    ((button == mouse.LEFT) and (modifiers & key.MOD_CTRL)):
+                # ON OSX, control + left click = right click.
+                if previous:
+                    self.model.add_block(previous, self.block)
+            elif button == pyglet.window.mouse.LEFT and block:
+                texture = self.model.world[block]
+                if texture != PEDRA:
+                    self.model.remove_block(block)
+        else:
+            self.set_exclusive_mouse(True)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+
+        if self.exclusive:
+            m = 0.15
+            x, y = self.rotation
+            x, y = x + dx * m, y + dy * m
+            y = max(-90, min(90, y))
+            self.rotation = (x, y)
+
+    def on_key_press(self, symbol, modifiers):
+
+        if symbol == key.W:
+            self.strafe[0] -= 1
+        elif symbol == key.S:
+            self.strafe[0] += 1
+        elif symbol == key.A:
+            self.strafe[1] -= 1
+        elif symbol == key.D:
+            self.strafe[1] += 1
+        elif symbol == key.SPACE:
+            if self.dy == 0:
+                self.dy = PULO_VELOCIDADE
+        elif symbol == key.ESCAPE:
+            self.set_exclusive_mouse(False)
+        elif symbol == key.TAB:
+            voar = not voar
+        elif symbol in self.num_keys:
+            index = (symbol - self.num_keys[0]) % len(self.inventory)
+            self.block = self.inventory[index]
+
+    def on_key_release(self, symbol, modifiers):
+
+        if symbol == key.W:
+            self.strafe[0] += 1
+        elif symbol == key.S:
+            self.strafe[0] -= 1
+        elif symbol == key.A:
+            self.strafe[1] += 1
+        elif symbol == key.D:
+            self.strafe[1] -= 1
+
+    def on_resize(self, width, height):
+
+        self.label.y = height - 10
+        if self.reticle:
+            self.reticle.delete()
+        x, y = self.width // 2, self.height // 2
+        n = 10
+        self.reticle = pyglet.graphics.vertex_list(4,
+            ('v2i', (x - n, y, x + n, y, x, y - n, x, y + n))
+        )
+
+    def set_2d(self):
+
+        width, height = self.get_size()
+        glDisable(GL_DEPTH_TEST)
+        viewport = self.get_viewport_size()
+        glViewport(0, 0, max(1, viewport[0]), max(1, viewport[1]))
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0, max(1, width), 0, max(1, height), -1, 1)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+    def set_3d(self):
+
+        width, height = self.get_size()
+        glEnable(GL_DEPTH_TEST)
+        viewport = self.get_viewport_size()
+        glViewport(0, 0, max(1, viewport[0]), max(1, viewport[1]))
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(65.0, width / float(height), 0.1, 60.0)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        x, y = self.rotation
+        glRotatef(x, 0, 1, 0)
+        glRotatef(-y, math.cos(math.radians(x)), 0, math.sin(math.radians(x)))
+        x, y, z = self.position
+        glTranslatef(-x, -y, -z)
+
+    def on_draw(self):
+
+        self.clear()
+        self.set_3d()
+        glColor3d(1, 1, 1)
+        self.model.batch.draw()
+        self.draw_focused_block()
+        self.set_2d()
+        self.draw_label()
+        self.draw_reticle()
+
+    def draw_focused_block(self):
+
+        vector = self.get_sight_vector()
+        block = self.model.hit_test(self.position, vector)[0]
+        if block:
+            x, y, z = block
+            vertex_data = cube_vertices(x, y, z, 0.51)
+            glColor3d(0, 0, 0)
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
+            pyglet.graphics.draw(24, GL_QUADS, ('v3f/static', vertex_data))
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+
+    def draw_label(self):
+
+        x, y, z = self.position
+        self.label.text = '%02d (%.2f, %.2f, %.2f) %d / %d' % (
+            pyglet.clock.get_fps(), x, y, z,
+            len(self.model._shown), len(self.model.world))
+        self.label.draw()
+
+    def draw_reticle(self):
+
+        glColor3d(0, 0, 0)
+        self.reticle.draw(GL_LINES)
+
+
+def setup_fog():
+
+    glEnable(GL_FOG)
+    glFogfv(GL_FOG_COLOR, (GLfloat * 4)(0.5, 0.69, 1.0, 1))
+    glHint(GL_FOG_HINT, GL_DONT_CARE)
+    glFogi(GL_FOG_MODE, GL_LINEAR)
+    glFogf(GL_FOG_START, 20.0)
+    glFogf(GL_FOG_END, 60.0)
+
+
+def setup():
+
+    glClearColor(0.5, 0.69, 1.0, 1)
+    glEnable(GL_CULL_FACE)
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+    setup_fog()
 
 
 def adicionar_bloco():
